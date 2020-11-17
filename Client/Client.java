cc6da49d4205f0cfa53bd8fd03355dc304ab9190
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-11-16 20:10:10 -0500

    added source [1]

---
+++ Client/Client.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+    static ReceiveMessages messagesThread;
+    static SendMessageThread sendMessageThread;
+    static Socket client;
+    public static void main(String[] args) {
+        try {
+            client = new Socket(InetAddress.getLocalHost(), 5050);
+            messagesThread = new ReceiveMessages(client);
+            messagesThread.start();
+
+            sendMessageThread = new SendMessageThread();
+            sendMessageThread.start();
+        }
+        catch(IOException e) {}
+    }
+
+    public static void getReceivedMessageFromServer(String message) {
+        System.out.println(message);
+    }
+
+    public static void sendMessage(String message) {
+        try {
+            PrintStream stream =  new PrintStream(client.getOutputStream());
+            stream.println(message);
+        }
+        catch (IOException e) {}
+    }
+}
